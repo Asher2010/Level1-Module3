5609b495efb4bd260496016c3cba6fc1c217bcfe
league <asherwelsbie@icloud.com>
2024-06-02 12:59:55 -0700

    end of typing tutor, start of plt

--- _02_gui_app_bindings/_b_TypingTutor/typing_tutor.py
+++ _02_gui_app_bindings/_b_TypingTutor/typing_tutor.py
@@ -10,25 +10,29 @@ def generate_random_letter():
 
 
 class TypingTutor(tk.Tk):
+
+
     def __init__(self):
         super().__init__()
 
         # TODO: Declare and initialize a member variable to hold a
         #  random letter to type
-        self.letter = ""
+
+        self.letter = generate_random_letter()
+
         # TODO: Declare and initialize a label (tk.Label) and set the text to the random letter
 
-        #self.label = tk.Label(self, text=letter, bg='green', font=('Rockwell', 20, 'normal', 'underline'))
+        self.label = tk.Label(self, text=self.letter, bg='green', font=('Rockwell', 20, 'normal', 'underline'))
         self.label.focus_set()
         self.label.bind('<KeyRelease>', self.on_key_release)
-        self.label.place(relx=0.5, rely=0.5, relwidth=0.5, relheight=0.5)
+        self.label.place(relx=0.0, rely=0.0, relwidth=1, relheight=1)
         # TODO: Place the label in the center of the window
 
         # TODO: Call the label's focus_set() method so key presses can be detected
 
         # TODO: Call the label's bind() method to call the on_key_release()
         #  method when a key is released
-        #  example: self.label.bind('<KeyRelease>', self.on_key_release)
+
 
 
     def on_key_release(self, event):
@@ -40,10 +44,15 @@ class TypingTutor(tk.Tk):
         #       If the user pressed the wrong key,
         #         change the label's background to red
         #  example: self.label.configure(bg='green')
+        if letter_pressed == self.letter:
+            self.label.configure(bg='green')
 
+        else:
+            self.label.configure(bg='red')
         # TODO: Get a new random letter and place it on the label
         #  example: self.label.configure(text=self.rand_letter)
-
+        self.letter = generate_random_letter()
+        self.label.configure(text=self.letter)
 
 if __name__ == '__main__':
     pass
--- _02_gui_app_bindings/_c_PigLatinTranslator/pig_latin_translator.py
+++ _02_gui_app_bindings/_c_PigLatinTranslator/pig_latin_translator.py
@@ -7,30 +7,41 @@ from PigLatinConverter import PigLatinConverter
 class PigLatinTranslator(tk.Tk):
     def __init__(self):
         super().__init__()
-
+        self.text = ''
         # TODO: Declare and initialize an Entry (tk.Entry) for the input text
-
+        self.entry = tk.Entry(self, textvariable=self.text, bg='yellow',
+                              fg='blue', font=('arial', 32, 'bold'), relief='solid')
+        self.entry.place(relx=0.0, rely=0.5, relwidth=0.4, relheight=0.3)
         # TODO: Declare and initialize a Button (tk.Button) that will translate
         #  the input text to pig latin when pressed
-
+        self.button = tk.Button(self, text='Translate', fg='blue',
+                                font=('courier new', 20, 'bold'))
+        self.button.place(relx=0.4, rely=0.5, relwidth=0.2, relheight=0.3)
         # TODO: Declare and initialize an label (tk.Label) for the translated
         #  text
-
+        self.label = tk.Label(self, text=self.text, bg='yellow',
+                              fg='blue', font=('arial', 32, 'bold'), relief='solid')
+        self.label.place(relx=0.6, rely=0.5, relwidth=0.4, relheight=0.3)
+        self.label.bind('<KeyRelease>', self.on_key_release)
         # TODO: Look at the example image () and place all the
         #  components in the same order
 
         # TODO: Call the label's bind() method to call the on_key_release()
         #  method when a key is released
 
+    def on_key_release(self, event):
 
-    def on_key_press(self, event):
+    #def on_key_press(self, event):
         print('button pressed!')
 
         # TODO: Use the _c_PigLatinTranslator.translate() method to translate
         #  the text in the input entry and set the text in the output entry
-
+        self.tt = self.text.translate()
 
 if __name__ == '__main__':
     pass
     # TODO: Create a new _c_PigLatinTranslator object and set the title and geometry.
     #  Remember to call mainloop() at the end
+    plt = PigLatinTranslator()
+    plt.geometry('1000x1000')
+    plt.mainloop()
